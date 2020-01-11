96362eb209083a8b18a7f601fbdc5afc9d3163f5
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-01-11 11:55:06 -0300

    Apply styles in the ChatRoom table.

--- src/ChatRoom/wwwroot/js/chat.js
+++ src/ChatRoom/wwwroot/js/chat.js
@@ -8,9 +8,22 @@ document.getElementById("sendButton").disabled = true;
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    td1.innerText = user;
+    var td2 = document.createElement("td");
+    td2.innerText = msg;
+    var td3 = document.createElement("td");
+    td3.innerText = "";
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    var list = document.getElementById("messagesList");
+    if (list.children.length > 0) {
+        list.insertBefore(tr, list.childNodes[0]);
+    } else {
+        list.appendChild(tr);
+    }
 });
 
 connection.start().then(function () {
@@ -22,7 +35,11 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    if (message === "")
+        return;
+    connection.invoke("SendMessage", user, message).then(function () {
+        document.getElementById("messageInput").value = "";
+    }).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
