98eadf5f90333e0f5447d4a8251d40c40c93961e
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2020-01-12 20:18:35 -0300

    Fix issue in the chat JS code.

--- src/ChatRoom/wwwroot/js/chat.js
+++ src/ChatRoom/wwwroot/js/chat.js
@@ -10,11 +10,14 @@ connection.on("ReceiveMessage", function (user, message, date) {
     var encodedMsg = user + " says " + msg;
     var tr = document.createElement("tr");
     var td1 = document.createElement("td");
+    td1.className = "date-col";
     td1.innerText = date;
     var td2 = document.createElement("td");
     td2.innerText = user;
+    td2.className = "user-col";
     var td3 = document.createElement("td");
     td3.innerText = msg;
+    td3.className = "message-col";
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
